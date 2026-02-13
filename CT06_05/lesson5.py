ef460c00f9c838ef67657cce6172de8e9db6af79
Computhink-CS143 <cs143@computhink.com.sg>
2026-02-13 10:54:31 +0000

    end of lesson 6

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -10,54 +10,54 @@ print("Hello from lesson 5")
 # print("Happy " + age + "th birthday, " + name + ". " + msg)
 
 #task 1
-yourname = input("give me a name: ")
-for letter in yourname:
-    print("give me a " + letter.upper(), "!")
+# yourname = input("give me a name: ")
+# for letter in yourname:
+#     print("give me a " + letter.upper(), "!")
 
-print(yourname + " is the best!")
-#task 2
-for count in range(100):
-    print("I like chinken rice.")
+# print(yourname + " is the best!")
+# #task 2
+# for count in range(100):
+#     print("I like chinken rice.")
 
-# task 3
-for count in range(100):
-    print("I like cake,")
-    print("give me more!")
+# # task 3
+# for count in range(100):
+#     print("I like cake,")
+#     print("give me more!")
 
-print("I am out of the loop!")
+# print("I am out of the loop!")
 
-#task 4
-for count in range(2, 101, 2):
-    print (count)
+# #task 4
+# for count in range(2, 101, 2):
+#     print (count)
 
-for count in range(0, 60, 1):
-    print (count)
+# for count in range(0, 60, 1):
+#     print (count)
 
-#task 5
-for count in range(1, 6, 1):
-    print (count)
+# #task 5
+# for count in range(1, 6, 1):
+#     print (count)
 
-for count in range(51, 101, 1):
-    print (count)
+# for count in range(51, 101, 1):
+#     print (count)
 
-for count in range(18, 30, 1):
-    print (count)
+# for count in range(18, 30, 1):
+#     print (count)
 
-#task 6
-for count in range(1, 6, 2):
-    print (count)
-
-
+# #task 6
+# for count in range(1, 6, 2):
+#     print (count)
 
 
 
 
 
 
+# task 8
+num = 0
+for count in range(10):
+    num = num + count
 
-
-
-
+print("the total is " + str(num))
 
 
 
--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,1 +1,424 @@
 print("Hello from lesson 6")
+
+# review
+# average = 0
+# total = 0
+# students = input("how many students do you have?")
+# students = int(students)
+
+# for count in range(1, students + 1):
+#     ques = "what is the marks of student " + str(count) + "? "
+#     marks = input(ques)
+#     total = total + int(marks)
+
+# average = total / students
+# print("the average marks for this class is ", round(average, 2))
+
+# Task 1a
+for i in range(3):
+    print("Hello, World!")
+
+# Task 1b
+for i in range(5):
+    print(i)
+
+# Task 1c
+print("Hello, World!")
+
+# **Task 1d**:
+num = 5
+
+# Task 1e
+print ("Hello, World!")
+# -----------------------------------------------
+# Task 2a
+print("age")
+
+# Task 2b
+name = "Alice"
+print(name)
+
+# Task 2c
+x = 5
+print(x)
+
+# **Task 2d**:
+print("Hello, World!")
+# -------------------------------
+# Task 3a
+age = 25
+print(age + 1)
+
+# **Task 3b**:
+number = 10
+print(number - 5)
+
+# Task 3c
+print("Repeat" * 3)
+
+# Task 3d
+year = 2023
+print("The year is ", year)
+
+# Task 3e
+x = 10
+y = x / 2
+
+# Task 3f
+end = 5
+for i in range(end):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
