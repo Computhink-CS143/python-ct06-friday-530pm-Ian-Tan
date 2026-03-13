4882808e233834d23c5f9cdfe5a21fd3c900926e
Computhink-CS143 <cs143@computhink.com.sg>
2026-03-13 10:51:18 +0000

    end of lesson 8

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,2 +1,175 @@
-print("Hello from lesson 8")
+# print("Hello from lesson 8")
+
+#qn2
+# piggybank = 0
+# startingAmount = int(input("what is the starting amount?"))
+# NumOfDays = int(input("how many days do you want to save it?"))
+# piggybank = piggybank + startingAmount
+# for count in range(1, NumOfDays+1):
+#     piggybank = piggybank + count
+#     print("day ", count, "= $", piggybank)
+
+# print("you have saved up $", piggybank)
+
+# review
+# product = 1
+
+# for count in range(1, 6):
+#     ans = input("what is number #" + str(count) + "?")
+#     ans = int(ans)
+#     product = product * ans
+
+# print("the final answer is " , product)
+
+#task 1a
+# import time
+# for count in range(10,0,-1):
+#     print(count)
+#     time.sleep(1)
+# print("time is up!")
+
+#task 1b
+# startTime = int(input("what number would you like to start with?"))
+# import time
+# for count in range(startTime, 0, -1):
+#     print(count)
+#     time.sleep(1)
+# print("time is up!")
+
+# #task 2a
+# import random
+# anum = random.randint(1, 6)
+# print(anum)
+
+# #task 2b
+# import random
+# for count in range(20):
+#     anum = random.randint(1, 9999)
+#     print(anum)
+
+#task 3a
+# iswalletempty = False
+
+# print(iswalletempty)
+
+# #task 3b
+# var1 = 678
+# var2 = 678
+# print(var1 == var2)
+
+#task 3c
+# israining = False
+# issunny = True
+# print(issunny == israining)
+
+#task 4
+# import random
+# random_num = random.randint(1, 3)
+# guess = int(input("what is the number?"))
+# if guess == random_num:
+#     print("you are correct!")
+# else:
+#     print("you are WRONG!!!!")
+
+#task 7
+num = int(input("what is the number?"))
+print(num % 2 == 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
