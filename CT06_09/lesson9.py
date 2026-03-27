b6ee68e38f76ac331f1b9a3b1152e7710ddf1c2e
Computhink-CS143 <cs143@computhink.com.sg>
2026-03-27 10:52:44 +0000

    end of lesson 9 and 10

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,1 +1,270 @@
-print("Hello from lesson 9")
+# print("Hello from lesson 9")
+
+# # recap
+# import random
+# num1 = random.randint(1,6)
+# num2 = random.randint(1,6)
+# num3 = random.randint(1,6)
+
+# print("1st number : " , num1)
+# print("2nd number : " , num2)
+# print("3rd number : " , num3)
+
+# evenOrOdd1 = num1 % 2 == 0
+# evenOrOdd2 = num2 % 2 == 0
+# evenOrOdd3 = num3 % 2 == 0
+# allEvenOdd = evenOrOdd1 == evenOrOdd2 == evenOrOdd3
+
+# print("All numbers are even/odd : " , allEvenOdd)
+
+# task 1
+# ....
+
+# task 2
+# days = int(input("how many days have you had that book?"))
+# if days > 25 :
+#     print("Remember to return your book!")
+
+# task 3b
+# how_many = int(input("how many apples do you want to buy?"))
+# if how_many > 10:
+#     print("You will get a 10% discount for buying that many apples!")
+#     price = how_many * 0.9
+#     print("the price is $" , price)
+# else:
+#     price = how_many * 1
+    # print("the price is $" , price)
+
+# task 4b
+price1 = 0
+price2 = 0
+many_apples = int(input("how many apples would you like to buy?"))
+many_oranges = int(input("how many oranges would you like to buy?"))
+if many_apples > 5:
+    price1 = 60 * many_apples / 0.9
+if many_oranges > 5:
+    price2 = 90 * many_oranges / 0.9
+total = price1 + price2
+print("the total you have to pay is " , round(total, 0) , "cents.")
+
+#draw.io
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,1 +1,383 @@
-print("Hello from lesson 10")
+# print("Hello from lesson 10")
+
+# recap
+# import random
+# randomNum = random.randint(1, 15)
+# guess = int(input("what number do you guess?"))
+# if guess == randomNum:
+#     print("that's correct!")
+# else:
+#     print("WRONG!!!")
+
+# task 1
+# num1 = int(input("give me a number."))
+# if num1 > 0:
+#     print(num1 , " is positive")
+# else:
+#     print(num1 , " is negitive")
+
+# task 2
+# age = int(input("what is your age?"))
+# if age < 13:
+#     print("you're a CHILD!")
+# else:
+#     if age < 20:
+#         print("you're a TEENAGER!")
+#     else:
+#         print("you're an ADULT!")
+
+# --------OR-----------
+
+# age = int(input("what is your age?"))
+# if 0 < age < 13:
+#     print("you're a CHILD!")
+# elif 13 <= age <= 20:
+#     print("you're a TEENAGER!")
+# else:
+#     print("you're an ADULT!")
+
+# task 3
+# temp = int(input("what is the temperature today?"))
+# if temp > 30:
+#     print("we should go SWIMMING!")
+# elif 25 < temp < 30:
+#     print("we should go  PLAY BASKETBALL!")
+# elif 20 < temp < 24:
+#     print("we should go CYCLING!")
+# else:
+#     print("we should go READ INDOORS!")
+
+# task 4
+score = int(input("what is your score?"))
+if score < 60:
+    print("you got a F!")
+elif score < 70:
+    print("you got a D!")
+elif score < 80:
+    print("you got a C!")
+elif score < 90:
+    print("you got a B!")
+else:
+    print("you got a A!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
