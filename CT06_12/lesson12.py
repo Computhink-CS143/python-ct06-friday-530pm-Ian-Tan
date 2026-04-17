a29103083e1f7d8ed81b99a83a19df9bf10f474f
Computhink-CS143 <cs143@computhink.com.sg>
2026-04-17 10:57:46 +0000

    end of lesson 11 and 12

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -1,1 +1,246 @@
-print("Hello from lesson 12")
+# print("Hello from lesson 12")
+
+# recap
+# game_status = "active"
+
+# if game_status == "active" or not game_status == "paused":
+#     print("game in progress...")
+# else:
+#     print("game is paused")
+
+# task 1a
+# visitors = 0
+# while visitors < 50:
+#     visitors = visitors + 1
+#     print(visitors)
+
+# task 1b
+# visitors = 18
+# while visitors < 30:
+#     visitors = visitors + 1
+#     print(visitors)
+
+# task 1c
+# visitors = 4
+# while visitors < 25:
+#     visitors = visitors + 1
+#     print(visitors)
+
+# task 2
+# visitors = 0
+# while visitors < 50:
+#     if visitors == 30:
+#         break
+#     visitors = visitors + 1
+#     print(visitors)
+
+# task 3
+# order = ""
+# reply = input("what is your order?")
+# while True:
+#     if reply == "end":
+#         break
+#     order = order + reply + ","
+#     reply = input("what is your order?")
+# print("your order is " , order)
+
+# task 4a
+# num1 = 10
+# while num1 > 0:
+#     print(num1)
+#     num1 = num1 - 1
+# else:
+#     print("Happy New Year!!!")
+
+# task 4b
+# num1 = 10
+# while True:
+#     print(num1)
+#     num1 = num1 - 1
+#     if num1 == 5:
+#         break
+# else:
+#     print("Happy New Year!!!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
