5381255e44480f82b343bdd7c6a371673be225c0
Computhink-CS143 <cs143@computhink.com.sg>
2026-04-24 10:53:27 +0000

    end of lesson 13

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,1 +1,121 @@
-print("Hello from lesson 13")
+# bonus
+# classmates = ["adrian",
+#               "ashton",
+#               "arya",
+#               "auster",
+#               "chirstopher",
+#               "issac",
+#               "isacc",
+#               "jude",
+#               "liam",
+#               "zachary",
+#               "edmund",
+#               "zhun zhe",
+#               "zhun kai",
+#               "matthias",
+#               "kegan",
+#               "david",
+#               "ian",
+#               "jasper",
+#               "hunter",
+#               "declan",
+#               "shane",
+#               "eliga",
+#               "emanual",
+#               "trevor",
+#               "shrihaan",
+#               "vohith",
+#               "jadon"
+#               ]
+
+# riddle
+ans = input("what do you call a bear without an ear?")
+
+if ans == "b" or ans == "B":
+    print("that is correct")
+else:
+    print("WRONG!!!!")
+# recap
+# counter = 25
+# while counter < 50:
+#     print(counter)
+#     counter = counter + 1
+
+# counter = 100
+# while counter > 79:
+#     print(counter)
+#     counter = counter - 1
+
+# counter = 5
+# while counter < 101:
+#     print(counter)
+#     counter = counter + 5
+
+# task 1a
+# groceries = ["apples", "bread", "carrots", "dates", "eggs", "flour", "grapes", "honey",]
+# for count in groceries:
+#     print(count)
+
+# task 1b
+# groceries = ["apples",
+#              "bread",
+#              "carrots",
+#              "dates",
+#              "eggs",
+#              "flour",
+#              "grapes",
+#              "honey"
+#              ]
+# groceries[7] = "herbs"
+# for count in range(len(groceries)):
+#     print(groceries[count])
+
+# task 1c
+# groceries = ["apples",
+#              "bread",
+#              "carrots",
+#              "dates",
+#              "eggs",
+#              "flour",
+#              "grapes",
+#              "honey"
+#              ]
+# groceries.insert(1, "bananas")
+# groceries.append("ice")
+# for count in groceries:
+#     print(count)
+
+# task 1d
+# groceries = ["apples",
+#              "bread",
+#              "carrots",
+#              "dates",
+#              "eggs",
+#              "flour",
+#              "grapes",
+#              "honey"
+#              ]
+# del(groceries[1])
+# for count in groceries:
+#     print(count)
+
+# task 2
+# groceries = ["apples",
+#              "bread",
+#              "carrots",
+#              "dates",
+#              "eggs",
+#              "flour",
+#              "grapes",
+#              "honey"
+#              ]
+# for count in groceries:
+#     print(count)
+#     if count == "apples":
+#         print(" I need 5 of these")
+#     if count == "carrots":
+#         print(" I need 3 of these")
+#     if count == "grapes":
+#         print(" Get the FarmFresh brand")
+
+# task 3
